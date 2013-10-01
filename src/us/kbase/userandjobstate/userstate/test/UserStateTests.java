98a2bc9b800bbe262df909921497b22f6f275590
Gavin Price <gaprice@lbl.gov>
2013-10-01 11:03:29 -0700

    tests

--- src/us/kbase/userandjobstate/userstate/test/UserStateTests.java
+++ src/us/kbase/userandjobstate/userstate/test/UserStateTests.java
@@ -7,11 +7,14 @@ import static org.junit.Assert.fail;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.JFrame;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import us.kbase.userandjobstate.test.UserJobStateTestCommon;
 import us.kbase.userandjobstate.userstate.UserState;
+import us.kbase.userandjobstate.userstate.exceptions.NoSuchKeyException;
 
 import com.mongodb.BasicDBObject;
 
@@ -34,22 +37,137 @@ public class UserStateTests {
 			us = new UserState(host, db, "userstate");
 		}
 	}
+	
+	@Test
+	public void illegalArgs() throws Exception {
+		checkArgs(null, "foo", true, "key", "user cannot be null or the empty string");
+		checkArgs(null, "foo", false, "key", "user cannot be null or the empty string");
+		checkArgs("", "foo", true, "key", "user cannot be null or the empty string");
+		checkArgs("", "foo", false, "key", "user cannot be null or the empty string");
+		checkArgs("u", null, true, "key", "service cannot be null or the empty string");
+		checkArgs("u", null, false, "key", "service cannot be null or the empty string");
+		checkArgs("u", "", true, "key", "service cannot be null or the empty string");
+		checkArgs("u", "", false, "key", "service cannot be null or the empty string");
+		checkArgs("u", "afa)aafe", true, "key", "Illegal character in service name afa)aafe: )");
+		checkArgs("u", "afae-afa", false, "key", "Illegal character in service name afae-afa: -");
+		checkArgs("u", "foo", true, null, "key cannot be null or the empty string");
+		checkArgs("u", "foo", false, null, "key cannot be null or the empty string");
+		checkArgs("u", "foo", true, "", "key cannot be null or the empty string");
+		checkArgs("u", "foo", false, "", "key cannot be null or the empty string");
+		checkListState(null, "foo", true, "user cannot be null or the empty string");
+		checkListState(null, "foo", false, "user cannot be null or the empty string");
+		checkListState("", "foo", true, "user cannot be null or the empty string");
+		checkListState("", "foo", false, "user cannot be null or the empty string");
+		checkListState("u", null, true, "service cannot be null or the empty string");
+		checkListState("u", null, false, "service cannot be null or the empty string");
+		checkListState("u", "", true, "service cannot be null or the empty string");
+		checkListState("u", "", false, "service cannot be null or the empty string");
+		checkListState("u", "afa)aafe", true, "Illegal character in service name afa)aafe: )");
+		checkListState("u", "afae-afa", false, "Illegal character in service name afae-afa: -");
+		checkListServices(null, true, "user cannot be null or the empty string");
+		checkListServices(null, false, "user cannot be null or the empty string");
+		checkListServices("", true, "user cannot be null or the empty string");
+		checkListServices("", false, "user cannot be null or the empty string");
+	}
+	
+	private void checkListState(String user, String service, boolean auth,
+			String exception) throws Exception {
+		try {
+			us.listState(user, service, auth);
+			fail("list state with bad args");
+		} catch (IllegalArgumentException iae) {
+			assertThat("correct exception", iae.getLocalizedMessage(),
+					is(exception));
+		}
+	}
+	
+	private void checkListServices(String user, boolean auth,
+			String exception) throws Exception {
+		try {
+			us.listServices(user, auth);
+			fail("list services with bad args");
+		} catch (IllegalArgumentException iae) {
+			assertThat("correct exception", iae.getLocalizedMessage(),
+					is(exception));
+		}
+	}
+	
+	private void checkArgs(String user, String service, boolean auth,
+			String key, String exception) throws Exception {
+		Object data = "foo";
+		try {
+			us.setState(user, service, auth, key, data);
+			fail("set state w/ bad args");
+		} catch (IllegalArgumentException iae) {
+			assertThat("correct exception", iae.getLocalizedMessage(),
+					is(exception));
+		}
+		try {
+			us.getState(user, service, auth, key);
+			fail("get state w/ bad args");
+		} catch (IllegalArgumentException iae) {
+			assertThat("correct exception", iae.getLocalizedMessage(),
+					is(exception));
+		}
+		try {
+			us.removeState(user, service, auth, key);
+			fail("remove state w/ bad args");
+		} catch (IllegalArgumentException iae) {
+			assertThat("correct exception", iae.getLocalizedMessage(),
+					is(exception));
+		}
+	}
+	
+	@Test
+	public void getNonExistantState() throws Exception {
+		us.setState("u", "nostate", false, "thing1", "foo");
+		us.setState("u", "nostate", true, "thing1", "foo");
+		try {
+			us.getState("u", "nostate", false, "thing");
+			fail("got non-existant state");
+		} catch (NoSuchKeyException iae) {
+			assertThat("correct exception", iae.getLocalizedMessage(),
+					is("There is no key thing for the unauthorized service nostate"));
+		}
+		try {
+			us.getState("u", "nostate", true, "thing");
+			fail("got non-existant state");
+		} catch (NoSuchKeyException iae) {
+			assertThat("correct exception", iae.getLocalizedMessage(),
+					is("There is no key thing for the authorized service nostate"));
+		}
+	}
 
 	@Test
 	public void getAndSetState() throws Exception {
 		Map<String, Object> data = new HashMap<String, Object>();
 		data.put("bar", "baz");
-		us.setState("foo", "serv1", false, "key1",
-				data);
+		us.setState("foo", "serv1", false, "key1", data);
 		assertThat("got correct data back",
 				us.getState("foo", "serv1", false, "key1"),
 				is((Object) new BasicDBObject(data)));
 		Map<String, Object> data2 = new HashMap<String, Object>();
 		data2.put("bar", data);
-		us.setState("foo", "authserv", true, "authkey", data2);
+		us.setState("foo", "authserv1", true, "authkey1", data2);
 		assertThat("got correct data back",
-				us.getState("foo", "authserv", true, "authkey"),
+				us.getState("foo", "authserv1", true, "authkey1"),
 				is((Object) new BasicDBObject(data2)));
+		us.setState("foo", "serv1", false, "key2", data);
+		us.setState("foo", "serv2", false, "key", data);
+		us.setState("foo", "authserv1", true, "authkey2", data2);
+		us.setState("foo", "authserv2", true, "authkey", data2);
+		//TODO finish basic ops testing
+	}
+	
+	@Test
+	public void setUnserializable() throws Exception {
+		try {
+			us.setState("foo", "unserializable", false, "a", new JFrame());
+			fail("saved unserializable object");
+		} catch (IllegalArgumentException iae) {
+			assertThat("correct exception", iae.getLocalizedMessage(),
+					is("Unable to serialize value"));
+		}
 	}
 	
 	private static final String BIG_STR = 
